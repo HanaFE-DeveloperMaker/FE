b83eb0bf242894ba9b297dd00eb6bae0d2c54e9f
address0 <soyong6624@gmail.com>
2025-03-18 19:48:12 +0900

    점수 출력 svg 구현

---
+++ js/FinalStats.js
@@ -0,0 +1,38 @@
+document.querySelectorAll(".donut-chart").forEach((chart) => {
+  let percent = chart.getAttribute("data-percent");
+  let circle = chart.querySelector(".circle");
+  let text = chart.querySelector(".percentage");
+  let overlay = chart.querySelector(".overlay");
+
+  let profileImg = chart.querySelector(".profile-img");
+
+  profileImg.setAttribute("href", "../assets/profile_success.png");
+
+  circle.style.strokeDasharray = `${percent}, 100`;
+  text.textContent = `${percent}%`;
+  let currentPercent = 0;
+  let interval = setInterval(() => {
+    if (currentPercent >= percent) {
+      clearInterval(interval);
+      setTimeout(() => {
+        overlay.classList.add("hide-overlay");
+        text.classList.add("hide-overlay");
+      }, 3000);
+    } else {
+      currentPercent++;
+      circle.style.strokeDasharray = `${currentPercent}, 100`;
+      text.textContent = `${currentPercent}%`;
+    }
+  }, 15);
+  chart.addEventListener("mouseenter", () => {
+    overlay.classList.remove("hide-overlay");
+    text.classList.remove("hide-overlay");
+  });
+
+  chart.addEventListener("mouseleave", () => {
+    if (currentPercent >= percent) {
+      overlay.classList.add("hide-overlay");
+      text.classList.add("hide-overlay"); // 애니메이션 후 오버레이 숨기기
+    }
+  });
+});
