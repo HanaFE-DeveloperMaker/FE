7f764a2353553befcb0e651332816b0bda1691a8
Jimin0304 <pjws0304@naver.com>
2025-03-21 09:10:28 +0900

    feat: 시작 화면 구현

--- js/start.js
+++ js/start.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const startButton = document.getElementById("startButton");
+  const nickname = document.querySelector("input");
 
   startButton.addEventListener("click", function () {
+    if (nickname.value.length == 0) {
+      alert('닉네임을 입력해주세요 !');
+      return ;
+    }
+
+    localStorage.setItem("nickname", nickname.value);
+
     window.location.href = "study.html";
   });
 });
